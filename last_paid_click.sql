4fea150fd421a7a9ae88d7277c9fbe1995ef8cdc
waynecs16-glitch <waynecs16@gmail.com>
2025-11-18 12:41:32 +0300

    last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,52 +1,51 @@
 WITH s_lpc AS (
     SELECT
-        s_inner.*,
-        l_inner.lead_id AS attributed_lead_id,
+        s_inner.visitor_id,
+        s_inner.visit_date,
+        s_inner.source,
+        s_inner.medium,
+        s_inner.campaign,
+        l_inner.lead_id,
+        l_inner.created_at,
+        l_inner.amount,
+        l_inner.closing_reason,
+        l_inner.status_id,
         ROW_NUMBER() OVER (
-            PARTITION BY l_inner.lead_id
+            -- ФИКС 1: Партиция по visitor_id
+            PARTITION BY s_inner.visitor_id
             ORDER BY s_inner.visit_date DESC
         ) AS rn
     FROM
         sessions AS s_inner
     INNER JOIN
         leads AS l_inner
-        ON s_inner.visitor_id = l_inner.visitor_id
+        ON
+            s_inner.visitor_id = l_inner.visitor_id
+            AND s_inner.visit_date <= l_inner.created_at
     WHERE
-        s_inner.medium IN (
-            'cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social'
-        )
-        AND s_inner.visit_date <= l_inner.created_at
+        s_inner.medium NOT IN ('organic')
 )
 
 SELECT
-    s.visitor_id,
-    s.visit_date,
-    l.lead_id,
-    l.created_at,
-    l.amount,
-    l.closing_reason,
-    l.status_id,
-    COALESCE(s_lpc.source, s.source) AS utm_source,
-    COALESCE(s_lpc.medium, s.medium) AS utm_medium,
-    COALESCE(s_lpc.campaign, s.campaign) AS utm_campaign
+    s_lpc.visitor_id,
+    s_lpc.visit_date,
+    s_lpc.source AS utm_source,
+    s_lpc.medium AS utm_medium,
+    s_lpc.campaign AS utm_campaign,
+    s_lpc.lead_id,
+    s_lpc.created_at,
+    s_lpc.amount,
+    s_lpc.closing_reason,
+    s_lpc.status_id
 FROM
-    sessions AS s
-LEFT JOIN
-    leads AS l
-    ON
-        s.visitor_id = l.visitor_id
-        AND s.visit_date <= l.created_at
-LEFT JOIN s_lpc
-    ON
-        s.visitor_id = s_lpc.visitor_id
-        AND s.visit_date = s_lpc.visit_date
-        AND s_lpc.rn = 1
+    s_lpc
 WHERE
-    l.lead_id IS NULL OR s_lpc.attributed_lead_id IS NOT NULL
+    s_lpc.rn = 1
 ORDER BY
-    l.amount DESC NULLS LAST,
-    s.visit_date ASC,
+    s_lpc.amount DESC NULLS LAST,
+    s_lpc.visit_date ASC,
     utm_source ASC,
     utm_medium ASC,
     utm_campaign ASC
 LIMIT 10;
+
