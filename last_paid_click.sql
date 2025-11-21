bed8bb2b2cd477acfaa3b78b9e6523486595d8f2
waynecs16-glitch <waynecs16@gmail.com>
2025-11-21 17:59:48 +0300

    last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,39 +1,59 @@
-WITH last_visits AS (
+WITH lpc AS (
     SELECT
-        visitor_id,
-        visit_date,
-        source,
-        medium,
-        campaign,
-        ROW_NUMBER() OVER (
-            PARTITION BY visitor_id
-            ORDER BY visit_date DESC
-        ) AS rn
-    FROM sessions
-    WHERE medium NOT IN ('organic')
+        s_lpc.visitor_id,
+        s_lpc.visit_date,
+        s_lpc.source,
+        s_lpc.medium,
+        s_lpc.campaign,
+        s_lpc.lead_id,
+        s_lpc.created_at,
+        s_lpc.amount,
+        s_lpc.closing_reason,
+        s_lpc.status_id
+    FROM (
+        SELECT
+            s.visitor_id,
+            s.visit_date,
+            s.source,
+            s.medium,
+            s.campaign,
+            l.lead_id,
+            l.created_at,
+            l.amount,
+            l.closing_reason,
+            l.status_id,
+            ROW_NUMBER() OVER (
+                PARTITION BY s.visitor_id
+                ORDER BY s.visit_date DESC
+            ) AS rn
+        FROM sessions AS s
+        INNER JOIN leads AS l
+            ON
+                s.visitor_id = l.visitor_id
+                AND s.visit_date <= l.created_at
+        WHERE s.medium NOT IN ('organic')
+    ) AS s_lpc
+    WHERE s_lpc.rn = 1
 )
+
 SELECT
-    lv.visitor_id,
-    lv.visit_date,
-    lv.source AS utm_source,
-    lv.medium AS utm_medium,
-    lv.campaign AS utm_campaign,
-    l.lead_id,
-    l.created_at,
-    l.amount,
-    l.closing_reason,
-    l.status_id
-FROM last_visits AS lv
-LEFT JOIN leads AS l
-    ON
-        lv.visitor_id = l.visitor_id
-        AND lv.visit_date <= l.created_at
-WHERE lv.rn = 1
+    lpc.visitor_id,
+    lpc.visit_date,
+    lpc.source AS utm_source,
+    lpc.medium AS utm_medium,
+    lpc.campaign AS utm_campaign,
+    lpc.lead_id,
+    lpc.created_at,
+    lpc.amount,
+    lpc.closing_reason,
+    lpc.status_id
+FROM lpc
 ORDER BY
-    l.amount DESC NULLS LAST,
-    lv.visit_date ASC,
-    lv.source ASC,
-    lv.medium ASC,
-    lv.campaign ASC
+    lpc.amount DESC NULLS LAST,
+    lpc.visit_date ASC,
+    lpc.source ASC,
+    lpc.medium ASC,
+    lpc.campaign ASC
 LIMIT 10;
+
 
